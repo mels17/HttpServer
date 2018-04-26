bccb9154cbd587e751a8ede77f642882e5ec11fe
Mel Vasu <malavika.vasudevan@myob.com>
2018-04-26 11:33:02 +1000

    Logic comments

---
+++ src/main/java/HttpServer.java
@@ -0,0 +1,15 @@
+public class HttpServer {
+    /*
+     What does a server have?
+     1. port number
+     2. localhost url
+     3. client listener
+     4. i. client sends a request to the server - get/post/put/delete
+        ii. server receives request
+        iii. routes request to request handler corresponding to request
+        iii. server then sends response
+     5. sockets - one endpoint in a two-way communication - need two sockets - server-side and client-side
+    */
+
+    public static final int PORT_NUMBER = 8000;
+}
